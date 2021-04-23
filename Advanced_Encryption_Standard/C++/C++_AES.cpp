a1f9e592320b951739bec292915f043b59634732
Jonathan Skopp <zeyecx@gmail.com>
2021-04-23 13:41:43 +0200

    AES

---
+++ Advanced_Encryption_Standard/C++/C++_AES.cpp
@@ -0,0 +1,8 @@
+#include <iostream>                                     
+#include <string>
+
+int main(){                                             
+    std::cout << "Hallo, du schöne Welt!" << std::endl; 
+
+    return 0;                                           
+}
