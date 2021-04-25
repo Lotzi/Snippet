0a568d5bd9b1deb765d848003b4cafd5e4a6afe2
Jonathan Skopp <zeyecx@gmail.com>
2021-04-25 21:35:54 +0200

    final Commit

--- No_Commit_Runner/no_commit.py
+++ No_Commit_Runner/no_commit.py
@@ -1,18 +1,41 @@
+import sys
+import re
 
-Path_to_read = "test_commit.txt"
-Trennzeichen = "#"
-Path_to_write = "test_fertig.txt"
+# Path_to_read [1]
+# Path_to_write [2]
+
+x = len(sys.argv[1])
+
+while x > 0:
+    if "." in sys.argv[1][-x::]:
+        x = x - 1
+        print(sys.argv[1][-x::])
+    else:
+        Trennzeichen = sys.argv[1][-x::]
+        break
 
 
-file_read = open(Path_to_read, "r+")
-file_write = open(Path_to_write, "r+")
+if x == "py":
+    Trennzeichen = "#"
+elif x == "pgn":
+    Trennzeichen = "%"
+elif x == "java":
+    Trennzeichen = "//"
+elif x == "text":
+    Trennzeichen = "#"
+else:
+    print("Unbekanntes Format")
+    quit()
+
+file_read = open(sys.argv[1], "r+")
+file_write = open(sys.argv[2], "r+")
 
 
 for line in file_read:
-    if Trennzeichen in line:
+    if sys.argv[3] in line:
         print("Delete: "+line)
-    else: 
-        file_write.write(line) 
+    else:
+        file_write.write(line)
 
 file_read.close()
 file_write.close()
