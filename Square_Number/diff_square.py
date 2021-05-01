976baa2132867c7d0c951d0a69e117d6fa08aa08
Jonathan Skopp <zeyecx@gmail.com>
2021-05-01 20:58:27 +0200

    grammar

--- Square_Number/diff_square.py
+++ Square_Number/diff_square.py
@@ -3,11 +3,11 @@
 figure = range(1, 1000)
 
 # List of all square numbers from the figures array.
-squere = []
+square = []
 
 # Generates the list of square numbers
 for i in figure:
-    squere.append(i**2)
+    square.append(i**2)
 
 # Maths output for lazy people
 symbol = '^2'
@@ -16,18 +16,18 @@ for i in figure:
     # When the array is finished, the programme terminates.
     # Then there is no annoying error message
     try:
-        dummy = squere[i]
+        dummy = square[i]
     except:
         quit()
 
     # The tuple consists of the entered numbers.
     tuple_figure = "(" + str(i) + "," + str(i - 1) + ")"+symbol+" -->"
-    tuple_squere = "(" + str(i**2) + "," + str((i - 1)**2) + ") :"
+    tuple_square = "(" + str(i**2) + "," + str((i - 1)**2) + ") :"
 
     # Generates the output for the console
-    diff = squere[i-1] - squere[i-2]
-    tuple_ausgabe = str(squere[i-1])+" - "+str(squere[i-2])
+    diff = square[i-1] - square[i-2]
+    tuple_ausgabe = str(square[i-1])+" - "+str(square[i-2])
 
     # Output
     if diff > 0:
-        print(tuple_figure, tuple_squere, tuple_ausgabe, " = ", diff)
+        print(tuple_figure, tuple_square, tuple_ausgabe, " = ", diff)
