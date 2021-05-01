8add279186293b675c43ac930f7d3af0ae6be466
Jonathan Skopp <zeyecx@gmail.com>
2021-05-01 20:34:04 +0200

    Quad Number ready

--- Square_Number/diff_squere.py
+++ Square_Number/diff_squere.py
@@ -1,14 +1,35 @@
-start = 1
-ende = 100
 
-diff = []
+# List of all positive integers from 0 to n
+figure = range(2, 101)
 
-# Legt die Liste an
-for x in range(0,100):
-    d =(x+1)**2 - x**2
-    diff.append(d)
+# List of all square numbers from the figures array.
+squere = []
 
+# Generates the list of square numbers
+for i in figure:
+    squere.append(i**2)
 
-# Test
-for y in diff:
-    print(y)
+# Maths output for lazy people
+symbol = '²'
+
+for i in figure:
+    # When the array is finished, the programme terminates.
+    # Then there is no annoying error message
+    try:
+        dummy = squere[i]
+    except:
+        quit()
+
+    # Template for the output
+    calc = squere[i]-squere[i-1]
+
+    # The tuple consists of the entered numbers.
+    tuple_figure = "(" + str(i) + "," + str(i - 1) + ")"+symbol+" -->"
+    tuple_squere = "(" + str(i**2) + "," + str((i - 1)**2) + ") :"
+
+    # Generates the output for the console
+    diff = squere[i] - squere[i-1]
+    tuple_ausgabe = str(squere[i])+" - "+str(squere[i-1])
+
+    # Output
+    print(tuple_figure, tuple_squere, tuple_ausgabe, " = ", diff)
