a09de8c48f31ed283dd90fc1a4e65ba1691587fa
Jonathan Skopp <zeyecx@gmail.com>
2021-05-04 21:43:41 +0200

    Format

--- Prime_number/Prime_tuple.py
+++ Prime_number/Prime_tuple.py
@@ -9,10 +9,10 @@ cap = sys.maxsize
 prime_figures = []
 
 # List of Prime Tuples
-for i in range(1, cap, 1):
-    if isprime(i) and isprime(i+3):
-        prime_figures.append((i, i+3))
+for i in range(1, 100000, 1):
+    if isprime(i) and isprime(i+2):
+        prime_figures.append((i, i+2))
 
 # Output
 for x in prime_figures:
-    print(str(x)+";",  end=" ")
+    print(x)
