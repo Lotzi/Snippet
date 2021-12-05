afeba3d2723760380a4669c09fe50d15b43a8b9e
Riffecx <riffecx@gmail.com>
2021-12-05 18:10:25 +0100

    .\Instagram\

--- Instagram/instagram.py
+++ Instagram/instagram.py
@@ -1,4 +1,9 @@
-import instaloader as insta
+import subprocess
+import os
+import sys
+user = "user"
+your_username = "your_username" # Optional
+
 
 """ 
 instaloader [--comments] [--geotags]
@@ -6,3 +11,38 @@ instaloader [--comments] [--geotags]
             [--login YOUR-USERNAME] [--fast-update]
             profile | "#hashtag" | :stories | :feed | :saved
 """
+
+# Download
+if user == "user":
+    print("Please enter a user.")
+    sys.exit(0)
+try:
+    subprocess.run("instaloader "+user, shell=True, check=True)
+
+    # Download with Login
+    # subprocess.run("instaloader --login="+your_username+" "+user, shell=True, check=True)
+except subprocess.CalledProcessError as e:
+    print(e.output)
+finally:
+    # CD to the downloaded folder
+    os.chdir(user)
+
+    # Delete JSON
+    directory = "./"
+    files_in_directory = os.listdir(directory)
+    filtered_files = [file for file in files_in_directory if file.endswith(".txt")]
+    for file in filtered_files:
+        path_to_file = os.path.join(directory, file)
+        os.remove(path_to_file)
+
+    # Delete RAR XZ
+    directory = "./"
+    files_in_directory = os.listdir(directory)
+    filtered_files = [file for file in files_in_directory if file.endswith(".xz")]
+    for file in filtered_files:
+        path_to_file = os.path.join(directory, file)
+        os.remove(path_to_file)
+
+    
+
+
