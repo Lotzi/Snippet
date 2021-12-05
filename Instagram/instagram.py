4f8226c585fba1c66397220318e8b696f58474a0
Riffecx <riffecx@gmail.com>
2021-12-05 17:39:49 +0100

    Upload

---
+++ Instagram/instagram.py
@@ -0,0 +1,8 @@
+import instaloader as insta
+
+""" 
+instaloader [--comments] [--geotags]
+            [--stories] [--highlights] [--tagged] [--igtv]
+            [--login YOUR-USERNAME] [--fast-update]
+            profile | "#hashtag" | :stories | :feed | :saved
+"""
